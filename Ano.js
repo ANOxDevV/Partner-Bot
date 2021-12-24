86adf0ba06feb54dcdbd6521a341c50b80be3e0e
7aMa0011 <96633598+7aMa0011@users.noreply.github.com>
2021-12-24 22:29:29 +0300

    Update Ano.js

--- Ano.js
+++ Ano.js
@@ -21,7 +21,7 @@ message.member.voiceChannel.join()
 
 } else { 
 
-message.reply('**<a:9FAF7C0CF0104A2F979F1263DF55D91A:855230044258828328> پێویستە تۆ لە ڤۆیسەکە بیت **'); 
+message.reply('**<a:9FAF7C0CF0104A2F979F1263DF55D91A:921358101117022208> پێویستە تۆ لە ڤۆیسەکە بیت **'); 
 
 } 
 
@@ -36,9 +36,9 @@ if(message.channel.type === 'dm') {
 
 if (message.content.startsWith("https://discord.gg/")) {
     
-  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/AkYA68Z2gG`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
+  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/fhPjyt25wq`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
     
-  client.channels.get("841792552314994709").send(
+  client.channels.get("921358110336098334").send(
 `> <a:E14567BBEE164342AF58DF6872011807:853287588390633502> By: <@${message.author.id}> ✅ 
 ${message.content}`)
 }
@@ -57,4 +57,4 @@ client.user.setGame(`PARTNER ANO`)
 
 
 
-client.login("");// Token Dane 
+client.login("OTAyNTU3MTc0Njg4MDIyNTU4.YXgJ3w.fEAkGscO3snwohWUONxJHHC0h3U");// Token Dane 
